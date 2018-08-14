a1b897d003f02278d1bae7b6c4fc5cb891d00074
Piniatyz <39610302+Piniatyz@users.noreply.github.com>
2018-08-14 14:46:17 +0700

    1
    1

---
+++ JS-2/js.js
@@ -0,0 +1,22 @@
+	
+		// alert("Welcome");
+		document.write("Hello this is javascript"+ "<br>");
+	
+
+	
+
+	var example="I am a variable!";
+	
+	document.write(example+"<br>");
+	example = "This is another exmaple for the same variable"
+	document.write(example);
+// var visitorage =prompt (" what is your age ?")
+//var visitorname = prompt(" what is your name ?");
+//var favcolor = prompt("What is your favorite color ?")
+ //document.write("hello" + visitorname +  " Your favorite Color is " + favcolor + "<br>");
+ //console.log("Program Complete");
+
+var hotText = "Google";
+var URL ='https://www.google.com/';
+document.write("<br>")
+document.write('Click to return to ' + hotText.link(URL));
